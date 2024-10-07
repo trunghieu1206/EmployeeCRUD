8b3dfb3aba1f739ef2c4eaa0945f944fa50fa426
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-10-08 00:15:47 +0700

    fix db-init.sql typo

--- init-scripts/db-init.sql
+++ init-scripts/db-init.sql
@@ -8,7 +8,7 @@ CREATE TABLE employee (
   id INT PRIMARY KEY NOT NULL,
   first_name varchar(45) DEFAULT NULL,
   last_name varchar(45) DEFAULT NULL,
-  email varchar(45) DEFAULT NULL,
+  email varchar(45) DEFAULT NULL
 );
 
 --

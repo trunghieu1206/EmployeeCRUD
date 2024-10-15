80ff9a68977d1551ff68e5451f1fa5eb02d9a7dc
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-10-15 22:14:32 +0700

    test

--- src/main/java/com/hieu/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/hieu/cruddemo/rest/EmployeeRestController.java
@@ -30,6 +30,7 @@ public class EmployeeRestController {
 
         if(theEmployee == null){
             throw new RuntimeException("Employee id not found - " + employeeId);
+            
         }
 
         return theEmployee;

5a8f13ecd26a216954be0c10f5927a4e933197fd
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-10-17 18:08:04 +0700

    fix

--- src/main/java/com/hieu/cruddemo/CruddemoApplication.java
+++ src/main/java/com/hieu/cruddemo/CruddemoApplication.java
@@ -2,12 +2,14 @@ package com.hieu.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+		System.out.println("My Spring version: " + SpringVersion.getVersion());
 	}
 
 }

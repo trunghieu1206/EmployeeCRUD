24fa97117e084bea0643c7e250478f8c4dfe8993
Hoàng Trung Hiếu <119180534+trunghieu1206@users.noreply.github.com>
2024-10-23 17:27:41 +0700

    Update db-init.sql

--- init-scripts/db-init.sql
+++ init-scripts/db-init.sql
@@ -1,11 +1,12 @@
--- 
+--
 -- Table structure for table employee
 --
 
 DROP TABLE IF EXISTS employees;
 
+-- use BIGSERIAL for auto increment ID
 CREATE TABLE employees (
-  id INT PRIMARY KEY NOT NULL,
+  id BIGSERIAL PRIMARY KEY NOT NULL,
   first_name varchar(45) DEFAULT NULL,
   last_name varchar(45) DEFAULT NULL,
   email varchar(45) DEFAULT NULL
@@ -15,10 +16,9 @@ CREATE TABLE employees (
 -- Data for table employee
 --
 
-INSERT INTO employees (id, first_name, last_name, email) VALUES (1, 'Leslie', 'Andrews', 'leslie@gmail.com');
-INSERT INTO employees (id, first_name, last_name, email) VALUES (2, 'Emma', 'Baumgarten', 'emma@gmail.com');
-INSERT INTO employees (id, first_name, last_name, email) VALUES (3, 'Avani', 'Gupta', 'avani@gmail.com');
-INSERT INTO employees (id, first_name, last_name, email) VALUES (4, 'Yuri', 'Petrov', 'yuri@gmail.com');
-INSERT INTO employees (id, first_name, last_name, email) VALUES (5, 'Juan', 'Vega', 'juan@gmail.com');
-
-
+-- When inserting do not specify ID part, postgres will handle this id part
+INSERT INTO employees (first_name, last_name, email) VALUES ('Leslie', 'Andrews', 'leslie@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Emma', 'Baumgarten', 'emma@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Avani', 'Gupta', 'avani@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Yuri', 'Petrov', 'yuri@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Juan', 'Vega', 'juan@gmail.com');
